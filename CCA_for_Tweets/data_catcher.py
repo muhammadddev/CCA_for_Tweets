4d004eaebce90783147c0391d88135415cd033ae
muhammadddev <35100767+muhammadddev@users.noreply.github.com>
2019-06-03 01:20:05 +0430

    Update data_catcher.py

--- CCA_for_Tweets/data_catcher.py
+++ CCA_for_Tweets/data_catcher.py
@@ -2,16 +2,16 @@ import re
 import json
 import tweepy
 
-access_token = "1046123176718200834-01QvgXnitZowONQtdpGqtzfFwUOjpg"
-access_token_secret = "FUyh1HsKxEd5SDxpq9VyJ40Ol6sP4IQMZPoDV5Rem7dDL"
-consumer_key = "7FVAVemGmTiJtDf4y2n4BFmeo"
-consumer_secret = "FNOMlByIjbYi7tD4nFQUzsJ8FRirXPAamKBRHX455A0YdeuRyO"
+access_token = "XXX"
+access_token_secret = "XXX"
+consumer_key = "XXX"
+consumer_secret = "XXX"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-user_home_timeline = api.user_timeline("muhammadddev")
+user_home_timeline = api.user_timeline("username")
 
 def save(date):
 	dates = open("dates.txt", "a+", encoding="utf-8") 
